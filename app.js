47f8c7b32e25b1b7b98f4dc1b0aecbe3dde71563
gustavohdev <g_a93@outlook.com>
2021-09-07 19:48:08 -0300

    added graphql query, es6 imports, running ok

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-const express = require("express")
-const app = express()
+import express from "express"
+import fetch from "node-fetch"
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 /* ... */
 
-app.get("/data", (req, res) => {
-    res.end("Response from the API!")
-  })
+app.get("/data", async (req, res) => {
+    //res.end("Response from the API!")
+    const query = `{ viewer { login } }`;
+    const url = "https://api.github.com/graphql";
 
-app.listen(3000, () => console.log("Server ready"))
+    const options = {
+        method: "post",
+        headers: {
+            "content-type": "application/json",
+            authorization: "bearer " + process.env.APIKEY,
+        },
+        body: JSON.stringify({ query: query }),
+    };
+
+    let response;
+    try {
+        response = await fetch(url, options);
+    } catch (error) {
+        console.error(error);
+    }
+    const data = await response.json();
+    res.json(data);
+    
+});
+
+app.listen(3000, () => console.log("Server ready"));
